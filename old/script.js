163f462cf9fdd006934c53203e07a4d49b9ece4a
callumjames90 <callum.clapton.2018@live.rhul.ac.uk>
2020-06-12 23:47:30 +0100

    Relocated item in prep for new project.

---
+++ old/script.js
@@ -0,0 +1,13 @@
+
+function updateClock() {
+
+    var now = new Date();
+
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+    var t = document.getElementById('clock-time');
+    t.innerHTML = now.toLocaleTimeString();
+
+    var d = document.getElementById('clock-date');
+    d.innerHTML = now.toLocaleDateString("en-US", options);
+}
